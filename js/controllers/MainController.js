976606ab39eea8b2e66b5ca4d805f38c481d3f53
Milan Grubnić <MilanG@aol.com>
2016-03-12 13:11:24 -0600

    refactored

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
-  
-  forecast.success(function(data){
-    $scope.fiveDay = data;
-  });
-  
+
+    forecast.success(function(data) {
+        $scope.fiveDay = data;
+    });
+
 }]);
